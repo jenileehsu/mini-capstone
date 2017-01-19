03f493809337e983a059ae04b39d0d221d910bae
jenilee <jenilee.hsu@gmail.com>
2017-01-18 21:03:18 -0800

    add buy button

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,14 @@
 class ProductsController < ApplicationController
 
   def get_products
+    
     @products = Product.all 
     render "products.html.erb"
+  end
+
+  def get_little_bee
+    @little_bee = Product.first
+    render "littlebee.html.erb"
   end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
 
   get "/products" => "products#get_products"
 
+  get "/littlebee" => "products#get_little_bee"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
