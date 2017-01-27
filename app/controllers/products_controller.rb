ebe3ddc4fa2d6071c5eb925f9feb034440b02e5b
jenilee <jenilee.hsu@gmail.com>
2017-01-26 20:40:07 -0800

    added background migrations sortby discountitems random

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,19 +1,63 @@
 class ProductsController < ApplicationController
 
+# code for 7 dwarves goes here (best practice)
+
   def index
     @products = Product.all 
+    if params[:sort]
+      @products = Product.all.order(params[:sort] => params[:sort_order])
+    end
+    
+    if params[:discount]
+      @products = Product.where("price < ?", 2)
+    end 
+
     render "index.html.erb"
   end
 
-  def show
-    products_id = params[:id]
-    @product = Product.find_by(id: products_id)
-    render = "show.html.erb"
+  def new
+    render "new.html.erb"
   end
 
-  def get_little_bee
-    @little_bee = Product.first
-    render "littlebee.html.erb"
+  def create
+    product = Product.create(
+      name: params[:name],
+      price: params[:price],
+      image: params[:image], 
+      description: params[:description])
+    flash[:success] = "Product successfully created!"
+    redirect_to "/products/#{product.id}"
+  end
+
+  def show
+    product_id = params[:id]
+    @product = Product.find_by(id: product_id)
+    if product_id == "random"
+      @product = Product.all.sample
+    end
+    render "show.html.erb"
+  end
+
+  def edit
+    product_id = params[:id]
+    @product = Product.find_by(id: product_id)
+    render "edit.html.erb"
+  end
+
+  def update
+   product_id = params[:id]
+   @product = Product.find_by(id: product_id)
+   @product.update(name: params[:name], description: params[:description], price: params[:price], image: params[:image])
+   flash[:success] = "Product successfully added!"
+   redirect_to "/products/#{@product.id}"
+  end
+
+  def destroy
+     product_id = params[:id]
+     @product = Product.find_by(id: product_id)
+     @product.destroy
+     flash[:warning] = "Product successfully deleted!"
+     redirect_to "/products"
   end
 
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,22 @@
 class Product < ApplicationRecord
+
+  def sale_message 
+    if price.to_f <= 2
+      
+      return "Discount Item!"
+    else
+      return "Everyday Value!!"
+    end
+  end
+
+  def tax
+    price.to_f * 0.09
+  end
+
+  def total
+    price.to_f + tax
+  end
+
 end
+
+#Bonus: add styling to your discounted items so the price shows up in red
